f522170f3c965576f7b9d7e5fae6b0d01c2804c1
sangyun0904 <sangyun0904@gmail.com>
2024-07-17 17:03:26 +0900

    add Taxi ride map to Enriched ride

--- datastream-api/src/main/java/com/example/DataPipelinesETL.java
+++ datastream-api/src/main/java/com/example/DataPipelinesETL.java
@@ -1,5 +1,9 @@
 package com.example;
 
+import com.example.common.datatypes.TaxiRide;
+import com.example.common.sources.TaxiRideGenerator;
+import com.example.common.utils.GeoUtils;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.co.RichCoFlatMapFunction;
@@ -24,7 +28,51 @@ public class DataPipelinesETL {
                 .flatMap(new ControlFunction())
                 .print();
 
+        DataStream<TaxiRide> rides = env.addSource(new TaxiRideGenerator());
+
+        DataStream<EnrichedRide> enrichedNYCRides = rides
+                .map(new Enrichment());
+
+        enrichedNYCRides.print();
+
         env.execute();
+    }
+
+
+    public static class EnrichedRide extends TaxiRide {
+        public int startCell;
+        public int endCell;
+
+        public EnrichedRide() {}
+
+        public EnrichedRide(TaxiRide ride) {
+            this.rideId = ride.rideId;
+            this.isStart = ride.isStart;
+            this.eventTime = ride.eventTime;
+            this.startLon = ride.startLon;
+            this.startLat = ride.startLat;
+            this.endLon = ride.endLon;
+            this.endLat = ride.endLat;
+            this.passengerCnt = ride.passengerCnt;
+            this.taxiId = ride.taxiId;
+            this.driverId = ride.driverId;
+            startCell = GeoUtils.mapToGridCell(ride.startLon, ride.startLat);
+            this.endCell = GeoUtils.mapToGridCell(ride.endLon, ride.endLat);
+        }
+
+        public String toString() {
+            return super.toString() + "," +
+                    this.startCell + "," +
+                    this.endCell;
+        }
+    }
+
+    public static class Enrichment implements MapFunction<TaxiRide, EnrichedRide> {
+
+        @Override
+        public EnrichedRide map(TaxiRide taxiRide) throws Exception {
+            return new EnrichedRide(taxiRide);
+        }
     }
 
 }
