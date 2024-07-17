633f2a3e0b272103706426977d05c6144910cbe0
sangyun0904 <sangyun0904@gmail.com>
2024-07-17 14:19:42 +0900

    add datastream Person adult filter

--- datastream-api/src/main/java/com/example/DataStreamJob.java
+++ datastream-api/src/main/java/com/example/DataStreamJob.java
@@ -18,6 +18,9 @@
 
 package com.example;
 
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
@@ -59,7 +62,37 @@ public class DataStreamJob {
 		 *
 		 */
 
+		DataStream<Person> flintstones = env.fromElements(
+				new Person("Fred", 35),
+				new Person("Wilma", 35),
+				new Person("Pebbles", 2)
+		);
+
+		DataStream<Person> adults = flintstones.filter(new FilterFunction<Person>() {
+			@Override
+			public boolean filter(Person person) throws Exception {
+				return person.age >= 18;
+			}
+		});
+
+		adults.print();
+
 		// Execute program, beginning computation.
-		env.execute("Flink Java API Skeleton");
+		env.execute();
+	}
+
+	public static class Person {
+		public String name;
+		public Integer age;
+		public Person() {}
+
+		public Person(String name, Integer age) {
+			this.name = name;
+			this.age = age;
+		}
+
+		public String toString() {
+			return this.name + ": age " + this.age;
+		}
 	}
 }
