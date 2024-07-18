8e188d397e8545d498d6854607a09f8f31a08622
sangyun0904 <sangyun0904@gmail.com>
2024-07-18 10:45:41 +0900

    add random words split counter window

---
+++ datastream-api/src/main/java/com/example/AnatomyOfFlinkProgram.java
@@ -0,0 +1,59 @@
+package com.example;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.time.Time;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.connector.source.util.ratelimit.RateLimiterStrategy;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.connector.datagen.source.DataGeneratorSource;
+import org.apache.flink.connector.datagen.source.GeneratorFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.util.Collector;
+
+import java.util.Random;
+
+public class AnatomyOfFlinkProgram {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        String[] words = {"Hello", "World", "Apache", "Flink", "Spark", "Spring", "Boot"};
+
+        GeneratorFunction<Long, String> generatorFunction = index -> "Number: " + words[new Random().nextInt(words.length)];
+        long numberOfRecords = 1000;
+
+        DataGeneratorSource<String> source =
+                new DataGeneratorSource<>(generatorFunction, numberOfRecords, RateLimiterStrategy.perSecond(17), Types.STRING);
+
+//        DataStreamSource<String> stream =
+//                env.fromSource(source,
+//                        WatermarkStrategy.noWatermarks(),
+//                        "Generator Source");
+//
+//        stream.print();
+
+        DataStream<Tuple2<String, Integer>> dataStream = env
+                .fromSource(source, WatermarkStrategy.noWatermarks(), "Generator Source")
+                .flatMap(new Splitter())
+                .keyBy(value -> value.f0)
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(5).toDuration()))
+                .sum(1);
+
+        dataStream.print();
+
+        env.execute("Window WordCount");
+    }
+
+    public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+        @Override
+        public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) throws Exception {
+            for (String word: sentence.split(" ")) {
+                out.collect(new Tuple2<String, Integer>(word, 1));
+            }
+        }
+    }
+}
